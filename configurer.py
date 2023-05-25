2d9b21c91e6a49ce8d1b30eacf80a7663ee56ed4
Andrea de Ruvo <a.deruvo@izs.it>
2023-05-25 15:30:57 +0200

    refactoring

--- configurer.py
+++ configurer.py
@@ -1,16 +1,18 @@
 import xmlschema
 import os.path
 from configuration import parse
+from constants import Constants
+from logger import Logger
 
 class Configurer:
-    base_schema_filename = "./configuration/schema_configuration.xsd"
-
+    base_schema_filename = Constants.CONFIGURER_BASE_XSD_SCHEMA
+    
     def __init__(self):
         if os.path.isfile(Configurer.base_schema_filename):
             self.schema = self.load_schema(Configurer.base_schema_filename)
-            print(f"[Success] {Configurer.base_schema_filename} loaded...")
+            Logger.s(f"{Configurer.base_schema_filename} loaded...")
         else:
-            print(f"[Error] {Configurer.base_schema_filename} doesn't exist...")
+            Logger.e(f"{Configurer.base_schema_filename} doesn't exist...")
 
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
@@ -19,24 +21,27 @@ class Configurer:
         if os.path.isfile(Configurer.base_schema_filename):
             return xmlschema.XMLSchema(xsd_filename)
         else:
-            print(f"[Error] {xsd_filename} doesn't exist...")
+            Logger.e(f"{xsd_filename} doesn't exist...")
+    
+    def load_base_configuration(self):
+        return self.load_configuration(Constants.CONFIGURATION_BASE_XML_FILE)
 
     def load_configuration(self, xml_filename):
         if os.path.isfile(xml_filename):
             if self.schema:
                 if self.is_valid(xml_filename):
-                    print(f"[Success] {Configurer.base_schema_filename} loaded...")
+                    Logger.s(f"{Configurer.base_schema_filename} loaded...")
                     return parse(xml_filename, silence=True)
                 else:
-                    print(f"[Error] {xml_filename} is not valid...")
+                    Logger.e(f"{xml_filename} is not valid...")
             else:
-                print(f"[Error] XML schema not loaded...")
+                Logger.e(f"XML schema not loaded...")
         else:
-            print(f"[Error] {xml_filename} doesn't exist...")
+            Logger.e(f"{xml_filename} doesn't exist...")
             
 
     def is_valid(self, xml_filename):
         if self.schema:
             return self.schema.is_valid(xml_filename)
         else:
-            print(f"[Error] XML schema not loaded...")
+            Logger.e(f"XML schema is not valid loaded...")
