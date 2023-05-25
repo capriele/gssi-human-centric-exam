7d2398cca919d64267e43c1ac335d1d9ce256b4a
Andrea de Ruvo <a.deruvo@izs.it>
2023-05-25 15:54:34 +0200

    refactoring

--- constants.py
+++ constants.py
@@ -1,8 +1,15 @@
 class Constants:
     
+    WINDOW_TITLE = "Human-Centric Simulation"
+    
     CONFIGURER_BASE_XSD_SCHEMA = "./configuration/schema_configuration.xsd"
     
     CONFIGURATION_BASE_XML_FILE = "./configuration/base_configuration.xml"
     
     ROBOT_INITIAL_NUMBER_OF_PILLS = 0
     ROBOT_INITIAL_DELTA = 0.5
+    
+    KEY_YES = "y"
+    KEY_NO = "n"
+    KEY_RESET = "r"
+    KEY_START = "s"
